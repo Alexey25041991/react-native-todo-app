44f587d4f8d785bfb690dff01fbf65f55f972f89
amazurenko <mazurenko.alexey@mail.ru>
2021-04-11 00:00:03 +0300

    Todo App

---
+++ src/components/Todo.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+export const Todo = ({ todo, onRemove, onOpen }) => {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.5}
+      onPress={() => onOpen(todo.id)}
+      onLongPress={() => onRemove(todo.id)}
+    >
+      <View style={styles.todo}>
+        <Text style={styles.text}>{todo.title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  todo: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 15,
+    borderWidth: 1,
+    borderColor: "#eee",
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  text: {
+    color: "#000",
+    fontSize: 20,
+  },
+});
